bed20f647e966e1bef27ab4de5bc7239eaf45abb
NIMISHA GEORGE <53266303+NimishaGeorge18@users.noreply.github.com>
2026-03-03 22:34:20 -0500

    Codepath

---
+++ README.md
@@ -0,0 +1,15 @@
+# CodePath TIP103 - Data Structures Practice
+
+This repository contains my weekly solutions for the CodePath TIP103 course.
+
+Topics covered:
+- Hash maps
+- Arrays
+- Sets
+- String manipulation
+- Problem solving patterns
+
+Structure:
+Week → Session → Problems
+
+Language: Python
---
+++ Week1/Session 2/Problem.py
@@ -0,0 +1,16 @@
+# Problem 5: Container with Most Honey
+# Christopher Robin is helping Pooh construct the biggest hunny jar possible.
+
+# Help him write a function that accepts an integer array heights of length n. The height of each element is given by heights[i].
+
+# There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, heights[i]).
+
+# Find two lines that, together with the x-axis, form the container that holds the most honey.
+
+# Return the maximum amount of honey a container can store.
+
+# Notice that you may not slant the container.
+
+# def most_honey(heights):
+# 	pass
+
