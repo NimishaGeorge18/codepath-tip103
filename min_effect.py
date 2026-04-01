ac0ad7226b67390674d0dc69927b2bd4154c71f3
NIMISHA GEORGE <53266303+NimishaGeorge18@users.noreply.github.com>
2026-03-31 21:41:08 -0400

    Question

--- min_effect.py
+++ min_effect.py
@@ -1,4 +1,21 @@
-# Buggy! Infinite Loop
+'''
+# Problem: Path with Minimum Effort
+
+You are a hiker preparing for an upcoming hike. You are given `heights`, a 2D array of size $rows \times columns$, where `heights[row][col]` represents the height of cell `(row, col)`. You are situated in the top-left cell, `(0, 0)`, and you hope to travel to the bottom-right cell, `(rows-1, columns-1)`.
+
+**The Goal:** Find a path that minimizes the **effort** required. Return the effort value for the best path possible.
+
+**The Constraints**
+
+- You can move **up**, **down**, **left**, or **right**.
+- A path's **effort** is the **maximum absolute difference** in heights between two consecutive cells of the path.
+
+**Example**
+
+- **Input:** `heights = [[1,2,2],[3,8,2],[5,3,5]]`
+- **Output:** `2`
+- **Explanation:** The path `[1,3,5,3,5]` has a maximum difference of 2, which is better than the "shorter" path `[1,2,2,2,5]` which has a maximum difference of 3.
+'''
 
 heights_1 = [
     [1,2,2],
